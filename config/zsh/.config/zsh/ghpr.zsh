9a155f8957e4498f3b3f2ce31493382d875273cc
Vebjorn Fjeldberg <vebjorn.fjeldberg@statnett.no>
2026-03-20 08:19:24 +0100

    Add copy url feature to ghpr

--- config/zsh/.config/zsh/ghpr.zsh
+++ config/zsh/.config/zsh/ghpr.zsh
@@ -3,7 +3,7 @@ ghpr() {
   fetch_cmd="bash $ZDOTDIR/scripts/ghpr-fetch.sh"
   fzf --ansi --prompt="Select PR > " \
     --delimiter='\t' --with-nth=1,2,3 \
-    --header=$'alt-o: open in web | alt-b: checkout | alt-a: approve | alt-m: merge | alt-r: reload'  \
+    --header=$'alt-o: open in web | alt-b: checkout | alt-a: approve | alt-m: merge | alt-c: copy URL | alt-r: reload'  \
     --preview "bash $ZDOTDIR/scripts/ghpr-preview.sh {4}" \
     --preview-window=up:75% \
     --phony --bind "start:reload($fetch_cmd)" \
@@ -11,7 +11,7 @@ ghpr() {
     --bind 'alt-b:execute(gh pr checkout {1})+abort' \
     --bind 'alt-o:execute-silent(gh pr view {1} --web)' \
     --bind 'alt-a:execute-silent(gh pr review {1} --approve)+reload('$fetch_cmd')' \
-    --bind 'alt-m:execute-silent(gh pr merge --squash {1})+reload('$fetch_cmd')'
+    --bind 'alt-m:execute-silent(gh pr merge --squash {1})+reload('$fetch_cmd')' \
+    --bind 'alt-c:execute-silent(gh pr view {1} --json url -q .url | xclip -selection clipboard)'
 
 }
-
