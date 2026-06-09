6089ef6e2e6cc94aa3fb5a48cc57fa235b1ae62d
Vebjorn Fjeldberg <vebjorn.fjeldberg@outlook.com>
2026-06-09 07:30:37 +0200

    Improve neovim searching

--- config/nvim/.config/nvim/lua/features/search.lua
+++ config/nvim/.config/nvim/lua/features/search.lua
@@ -7,6 +7,12 @@ function M.setup()
   vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
   vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
   vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+  vim.keymap.set('n', '<leader>sF', function()
+    builtin.find_files {
+      hidden = true,
+      no_ignore = true,
+    }
+  end, { desc = '[S]earch all [F]iles' })
   vim.keymap.set('n', '<leader>st', function()
     builtin.find_files {
       cwd = vim.fn.getcwd() .. '/tmp',
@@ -17,6 +23,13 @@ function M.setup()
   vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
   vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
   vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+  vim.keymap.set('n', '<leader>sG', function()
+    builtin.live_grep {
+      additional_args = function()
+        return { '--hidden' }
+      end,
+    }
+  end, { desc = '[S]earch hidden by [G]rep' })
   vim.keymap.set('n', '<leader>sT', function()
     builtin.live_grep {
       cwd = vim.fn.getcwd() .. '/tmp',
--- config/tmux/.config/tmux/scripts/jira-mywork-preview.sh
+++ config/tmux/.config/tmux/scripts/jira-mywork-preview.sh
@@ -19,16 +19,57 @@ if [[ -z "$b64" ]]; then
 fi
 
 printf '%s' "$b64" | base64 --decode | jq -r --arg last_sync "$last_sync" '
-  def textify:
-    if type == "string" then .
-    elif type == "array" then map(textify) | join("")
-    elif type == "object" then
-      if has("text") then .text
-      elif has("content") then (.content | textify)
-      else ""
+  def repeat($s; $n):
+    reduce range(0; $n) as $i (""; . + $s);
+
+  def marks_text($node):
+    reduce (($node.marks // []))[] as $mark ($node.text // "";
+      if $mark.type == "link" then . + " <" + ($mark.attrs.href // "") + ">"
+      elif $mark.type == "code" then "`" + . + "`"
+      else .
       end
-    else ""
+    );
+
+  def inline_text:
+    if type == "string" then .
+    elif type == "array" then map(inline_text) | join("")
+    elif type != "object" then ""
+    elif .type == "text" then marks_text(.)
+    elif .type == "hardBreak" then "\n"
+    elif .type == "mention" then .attrs.text // .attrs.id // ""
+    elif .type == "emoji" then .attrs.text // .attrs.shortName // ""
+    elif .type == "inlineCard" then .attrs.url // ""
+    elif .type == "status" then .attrs.text // ""
+    else (.content // []) | map(inline_text) | join("")
     end;
+
+  def blocks($indent; $ordered):
+    if type == "array" then map(blocks($indent; $ordered)) | join("")
+    elif type != "object" then ""
+    elif .type == "doc" then (.content // []) | blocks($indent; $ordered)
+    elif .type == "paragraph" then ((.content // []) | inline_text) + "\n"
+    elif .type == "heading" then ((.content // []) | inline_text) + "\n"
+    elif .type == "bulletList" then (.content // []) | map(blocks($indent; false)) | join("")
+    elif .type == "orderedList" then (.content // []) | to_entries | map(.value | blocks($indent; .key + 1)) | join("")
+    elif .type == "listItem" then
+      ((if $ordered then ($ordered|tostring) + ". " else "- " end) as $prefix
+      | ((.content // []) | map(blocks($indent + ($prefix | length); false)) | join("")) as $body
+      | ($body | sub("\n$"; "") | split("\n")) as $lines
+      | if ($lines | length) == 0 or ($lines[0] == "") then ""
+        else
+          ($prefix + $lines[0] + "\n")
+          + (($lines[1:] | map(select(length > 0) | repeat(" "; $indent + ($prefix | length)) + . + "\n")) | join(""))
+        end)
+    elif .type == "codeBlock" then ((.content // []) | inline_text) + "\n"
+    elif .type == "rule" then "---\n"
+    elif .type == "blockquote" then ((.content // []) | blocks($indent; $ordered) | split("\n") | map(select(length > 0) | "> " + .) | join("\n")) + "\n"
+    else ((.content // []) | map(blocks($indent; false)) | join(""))
+    end;
+
+  def render_description:
+    (blocks(0; false)
+      | gsub("\n{3,}"; "\n\n")
+      | sub("\n+$"; ""));
 
   . as $issue |
   [
@@ -46,7 +87,7 @@ printf '%s' "$b64" | base64 --decode | jq -r --arg last_sync "$last_sync" '
       if ($issue.fields.description? // null) == null then
         ""
       else
-        "\nDescription\n" + (($issue.fields.description.content // []) | textify)
+        "\nDescription\n" + (($issue.fields.description // {}) | render_description)
       end
     )
   ]
